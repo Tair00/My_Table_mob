9c0db606a5843a81db66c76d0318be27d3298579
Tair00 <suleimanov.t.i.20@gmail.com>
2023-06-10 15:39:26 +0300

    br4

--- app/src/main/java/ru/mvlikhachev/mytablepr/Auth/LoginResult.java
+++ app/src/main/java/ru/mvlikhachev/mytablepr/Auth/LoginResult.java
@@ -1,13 +1,19 @@
 package ru.mvlikhachev.mytablepr.Domain;
 
-public class LoginResult {
+import com.google.gson.annotations.SerializedName;
 
+public class LoginResult {
+    @SerializedName("access_token")
+    private String token;
     private String name;
 
     private String password;
 
     public String getName() {
         return name;
+    }
+    public String getToken() {
+        return token;
     }
 
     public String getPassword() {
